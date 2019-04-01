e3410605c56efef0a578bbee58e925b8313af503
mjesar <mohammadalijaisar@gmail.com>
2019-04-01 20:12:28 +0500

    condition check if method is Post or get and return jsonfy

--- app.py
+++ app.py
@@ -12,8 +12,16 @@ app = Flask(__name__)
 @app.route('/data/api/v1.0/test_data',methods=['POST','GET',])
 def test_data():
 
+    if request.method == 'POST':
 
-    return jsonify({"Test Data ":"data"})
+
+        return jsonify({"POST Method ":"data"})
+
+    elif request.method == 'GET':
+
+        
+
+        return jsonify({"GET Method":"Data"})
 
 
 
