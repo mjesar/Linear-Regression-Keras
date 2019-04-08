e58faf3727d9e467d4699f2537e0dd7fb59eef16
mjesar <mohammadalijaisar@gmail.com>
2019-04-08 21:19:09 +0500

    save data that is entered and predition value with index to a csv file

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from keras.models import load_model
 from keras import backend as K
-
+import pandas as pd
+import json
 import numpy as np
 
 
@@ -18,23 +19,50 @@ def test_data():
         data = request.get_json(force=True)
 
         # get data from json post request
-        X_new = data['test_data']
+        X_new_json = data['test_data']
 
         # load saved model
         trained_model = load_model('trained_model.h5')
 
-        # Convert data to numoy array
-        X_new = np.array([X_new])
+        # Convert data to numpy array
+        X_new = np.array([X_new_json])
         print(X_new)
 
         # from trained model predict Y values
         y_new = trained_model.predict(X_new)
 
+        #after predicting my data i inserted this part of code then i had again loaded the model
         K.clear_session()
+
+        # read and get length of complete rows
+        dfObj = pd.read_csv("Predictions.csv")
+        len_index = len(dfObj['ID'])
+        # print(len_index+1)
+
+        dict = {}
+
+        # append data to dictnary
+        dict.update({"ID": [len_index + 1], "Data": [X_new[0]], "Predicted": [y_new[0]]})
+
+        # convert it to dataframe to can put to csv
+        df = pd.DataFrame(dict)
+
+        # open csv file flag "a" to append data and header which is columns would be only one time  bcs of header=f.tell() == 0
+        with open("Predictions.csv", 'a') as f:
+            df.to_csv(f, mode='a', index=False, header=f.tell() == 0)
+
 
         #print values inputed and pridected
         print("X=%s, Predicted=%s" % (X_new[0], y_new[0]))
-        return jsonify({"POST Method ":""})
+
+        #x_new_json = json.loads(x_new_json[0].to_json(orient='records'))
+        #y_new_json = json.loads(y_new[0].to_json(orient='records'))
+        #print(y_new_json)
+
+
+        return jsonify({X_new_json:""})
+
+
 
     elif request.method == 'GET':
 
